1ec1de18b08b6ea9fccce7c67e86c7c132bed14c
murliv-design <immurlivijay@gmail.com>
2026-03-03 15:15:46 +0530

    Add files via upload

---
+++ mailTemplate.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="format-detection" content="date=no">
+            <meta name="format-detection" content="telephone=no"/>
+            <meta name="x-apple-disable-message-reformatting">
+                <title>Vivahalive</title>
+                <!--[if mso]>
+                <style>
+                * {font-family: Arial, Helvetica, sans-serif !important;}
+                </style>
+                <![endif]-->
+                <!--[if !mso]><!-->
+                <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,300&subset=latin,cyrillic,greek" rel="stylesheet" type="text/css">
+                    <!--<![endif]-->
+                    <style type="text/css">
+
+
+                        .ReadMsgBody { width: 100%; background-color: #ffffff;}
+                        .ExternalClass {width: 100%; background-color: #ffffff;}
+                        .ExternalClass, .ExternalClass p, .ExternalClass span,
+                        .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height:100%;}
+                        #outlook a { padding:0;}
+                        html,body {margin: 0 auto !important; padding: 0 !important; height: 100% !important; width: 100% !important; background-color: #ffffff;}
+                        * {-ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;}
+                        table,td {mso-table-lspace: 0pt !important; mso-table-rspace: 0pt !important;}
+                        table {border-spacing: 0 !important;}
+                        table table table {table-layout: auto;}
+                        table td {border-collapse:collapse;}
+                        table p {margin:0;}
+                        img {height: auto !important; line-height: 100%; outline: none; text-decoration: none !important; -ms-interpolation-mode:bicubic;}
+                        br, strong br, b br, em br, i br { line-height:100%; }
+                        div, p, a, li, td { -webkit-text-size-adjust:none; -ms-text-size-adjust:none;}
+                        h1, h2, h3, h4, h5, h6 { line-height: 100% !important; -webkit-font-smoothing: antialiased; }
+                        span a ,a { text-decoration: none !important;}
+                        .yshortcuts, .yshortcuts a, .yshortcuts a:link,.yshortcuts a:visited,
+                        .yshortcuts a:hover, .yshortcuts a span { text-decoration: none !important; border-bottom: none !important;}
+
+                        /*mailChimp class*/
+                        .default-edit-image{height:20px;}
+                        ul{padding-left:10px; margin:0;}
+                        .tpl-repeatblock {padding: 0px !important; border: 1px dotted rgba(0,0,0,0.2);}
+                        .tpl-content {padding:0px !important;}
+
+
+                        @media only screen and (max-width:640px){
+                            .full-width,.container{width:95%!important; float:none!important; min-width:95%!important; max-width:95%!important; margin:0 auto!important; padding-left:15px; padding-right:15px; text-align: center!important; clear: both;}
+                            #mainStructure, #mainStructure .full-width .full-width,table .full-width .full-width, .container .full-width{width:100%!important; float:none!important; min-width:100%!important; max-width:100%!important; margin:0 auto!important; clear: both; padding-left:0; padding-right:0;}
+                            .no-pad{padding:0!important;}
+                            .full-block{display:block!important;}
+                            .image-full-width,
+                            .image-full-width img{width:100%!important; height:auto!important; max-width:100%!important;}
+                            .full-width.fix-800{min-width:auto!important;}
+                            .remove-block{display:none !important;}
+                            .pad-lr-20{padding-left:20px!important; padding-right:20px!important;}
+                        }
+
+                        @media only screen and (max-width:480px){
+                            .full-width,.container{width:95%!important; float:none!important; min-width:95%!important; max-width:95%!important; margin:0 auto!important; padding-left:15px; padding-right:15px; text-align: center!important; clear: both;}
+                            #mainStructure, #mainStructure .full-width .full-width,table .full-width .full-width,.container .full-width{width:100%!important; float:none!important; min-width:100%!important; max-width:100%!important; margin:0 auto!important; clear: both; padding-left:0; padding-right:0;}
+                            .no-pad{padding:0!important;}
+                            .full-block{display:block!important;}
+                            .image-full-width,
+                            .image-full-width img{width:100%!important; height:auto!important; max-width:100%!important;}
+                            .full-width.fix-800{min-width:auto!important;}
+                            .remove-block{display:none !important;}
+                            .pad-lr-20{padding-left:20px!important; padding-right:20px!important;}
+                        }
+
+                        td ul{list-style: initial; margin:0; padding-left:20px;}
+                        body{background-color:#ffffff; margin: 0 auto !important; } .default-edit-image{height:20px;} tr.tpl-repeatblock , tr.tpl-repeatblock > td{ display:block !important;} .tpl-repeatblock {padding: 0px !important;border: 1px dotted rgba(0,0,0,0.2); }
+                        @media only screen and (max-width: 640px){ .row{display:table-row!important;} .image-100-percent{ width:100%!important; height: auto !important; max-width: 100% !important; min-width: 124px !important;}}
+                        @media only screen and (max-width: 480px){ .row{display:table-row!important;}}
+
+                        *[x-apple-data-detectors],
+                        .unstyle-auto-detected-links *,
+                        .aBn{border-bottom: 0 !important; cursor: default !important;color: inherit !important; text-decoration: none !important;font-size: inherit !important; font-family: inherit !important; font-weight: inherit !important;line-height: inherit !important;}
+                        .im {color: inherit !important;}
+                        .a6S {display: none !important; opacity: 0.01 !important;}
+                        img.g-img + div {display: none !important;}
+                        a img{ border: 0 !important;}
+                        a:active{color:initial } a:visited{color:initial }
+                        span a{color:inherit;}
+                        .tpl-content{padding:0 !important;}
+                        table td ,table th{border-collapse:collapse; display:table-cell!important;}
+                        table,td,th, img{min-width:0!important;}
+                        #mainStructure{padding:0 !important;}
+                        span,p{display:inline!important;}
+                        .row{display:flex;}
+                    </style>
+                    <!--[if mso]>
+                    <style type="text/css">
+                    body { font-size: 0; line-height: 0; }
+                    table,tr { font-size:1px; mso-line-height-alt:0; line-height:0; mso-margin-top-alt:1px;}
+                    body,table,td,span,a,font{font-family: Arial, Helvetica, sans-serif!important; mso-line-height-rule:exactly; -ms-text-size-adjust:100%; -webkit-text-size-adjust:100%;}
+                    a img{ border: 0 !important;}
+                    a{text-decoration: none !important;}
+                    table td ,table th{border-collapse:collapse; display:table-cell!important;}
+                    table,td,th, img{min-width:0!important;}
+                    </style>
+                    <![endif]-->
+                    <!--[if gte mso 9]><xml>
+                    <o:OfficeDocumentSettings>
+                    <o:AllowPNG/>
+                    <o:PixelsPerInch>96</o:PixelsPerInch>
+                    </o:OfficeDocumentSettings>
+                    </xml><![endif]-->
+                    <!--[if mso]>
+                    <xml xmlns:w="urn:schemas-microsoft-com:office:word">
+                    <w:WordDocument><w:AutoHyphenation/></w:WordDocument>
+                    </xml>
+                    <![endif]-->
+                    </head>
+                    <body  style="font-size:12px; width:100%; height:100%;">
+                        <div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
+                            ‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;‌&nbsp;
+                        </div>
+                        <table id="mainStructure" class="full-width" width="600" align="center" border="0" cellspacing="0" cellpadding="0" style="background-color: #ffffff; max-width: 600px; outline: rgb(239, 239, 239) solid 1px; box-shadow: rgb(224, 224, 224) 0px 0px 30px 5px; margin: 10px auto 10px auto;" bgcolor="#ffffff">
+                            <!-- START ORDER-TABLE -->
+                            <tr>
+                                <td valign="top" align="center" style="background-color: #ffffff; " bgcolor="#ffffff">
+                                    <table width="600" align="center" border="0" cellspacing="0" cellpadding="0" class="full-width" style="background-color: #ffffff; margin: 0px auto; width: 600px; min-width: 320px; max-width: 90%;" role="presentation" bgcolor="#ffffff">
+                                        <tr>
+                                            <td valign="top" align="center">
+                                                <table width="590" align="center" border="0" cellspacing="0" cellpadding="0" class="full-width" style="margin: 0px auto; width: 590px; min-width: 280px; max-width: 90%;" role="presentation">
+                                                    <!-- start title -->
+                                                    <tr dup="0">
+                                                        <td valign="top">
+                                                            <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="margin: 0px auto; min-width: 100%;" role="presentation">
+                                                                <!-- start space -->
+                                                                <tr>
+                                                                    <td valign="top" height="20" style="height: 20px; font-size: 0px; line-height: 0;" aria-hidden="true">&nbsp;</td>
+                                                                </tr><!-- end space -->
+                                                                <tr>
+                                                                    <td align="center" valign="top" width="600" style="width:100%;line-height:0px;" mc:edit="image-0">
+                                                                        <img src="https://www.vivahalive.com/assets/img/logo_vivahalive_svg.svg" width="240" style="display: block !important; width:240px; max-width: 240px; margin: 0px 0px 0px 0px;" vspace="0" hspace="0" alt="image">
+                                                                    </td>
+                                                                </tr><!-- start space -->
+                                                              
+                                                            </table>
+                                                        </td>
+                                                    </tr><!-- end title -->
+                                                    <tr dup="0">
+                                                        <td valign="top" align="center">
+                                                            <table width="100%" align="center" border="0" cellspacing="0" cellpadding="0" style="margin: 0px auto; min-width: 100%;" role="presentation">
+                                                                <!-- start space -->
+                                                                <tr>
+                                                                    <td valign="top" height="15" style="height: 15px; font-size: 0px; line-height: 0;" aria-hidden="true">&nbsp;</td>
+                                                                </tr><!-- end space -->
+                                                                <tr  style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">
+                                                                    <h1  style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 18px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Hello NAME,</h1><br>
+                                                                        <p  style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Thank you for reaching out to us.</p><br/>
+                                                                        <p style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Our sales department will contact you to confirm your meeting request shortly.</p><br>
+                                                                                <p style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Please do not reply to this message as we will not receive it.</p><br><br>
+                                                                                <p style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Best regards,</p><br/>
+                                                                                <p style="font-style: normal; text-align: left; color: #000; line-height: 24px; font-size: 14px; font-weight: 400; font-family: 'Open Sans', Arial, Helvetica, sans-serif;">Vivahalive Sales Department</p>
+                                                                </tr><!-- start space -->
+                                                                <tr>
+                                                                    <td valign="top" height="20" style="height: 20px; font-size: 0px; line-height: 0;" aria-hidden="true">&nbsp;</td>
+                                                                </tr><!-- end space -->
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                    
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr><!-- END ORDER-TABLE -->
+                        </table></body>
+                    </html>
